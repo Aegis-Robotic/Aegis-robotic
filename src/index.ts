83a550a333c332b5d756e28fb3dcf18ba0614f95
AEGIS <hbikehbk@gmail.com>
2026-03-04 04:22:59 +0100

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Business Source License 1.1 - Modified Version
+ * Change Date: 2030-03-04
+ * Copyright (c) 2026 - [Entity Protected - Ownership intentionally obscured for strategic IP protection]
+ */
+
+export { AegisGuard } from './AegisGuard';
+export type { AegisConfig, GuardedAgent } from './types';
